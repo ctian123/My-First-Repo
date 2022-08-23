f4c7cbc7019462ce3e493addc6116dec5e72747c
ctian123 <ctian@stu.naperville203.org>
2022-08-23 14:19:39 -0500

    BlueJ and Notes

---
+++ My First BlueJ Project/ch1Notes.java
@@ -0,0 +1,44 @@
+
+/**
+ * Chungtai Tian
+ * 8/23/22
+ * Chapter 1 Notes
+ * Notes from class
+ * Who helped me:
+ * 
+ */
+
+import java.util.Scanner;//class is last arg
+import java.util.Random;
+
+public class ch1Notes
+{
+    /**
+     * this is code notes for ch 1
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        // basic data types
+        int x = 9, y, z;
+        boolean choice = false;
+        char letter = 'c';
+        float number2 = 9;
+        double number = 8.9, number3, number4;
+        short num = 6;
+        long numberrr = 9999;
+        
+        //These are Objects
+        //Class  id       memory  Constructor
+        String   word =   new     String("Chungtai Tian");
+        String   name =   "John";
+        Integer  cumber = new     Integer(47);
+        Double   dumber = new     Double(45.8);
+        
+        System.out.println(name);
+    }
+}
